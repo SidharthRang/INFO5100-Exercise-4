273c87ef99355e454dde1326dbe94b75d729b813
SidharthRang <sidrang.sr@gmail.com>
2021-11-12 16:46:18 -0800

    Third commit

--- src/App.java
+++ src/App.java
@@ -4,10 +4,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         boolean m, n, o, p;
 
-        String[][] input = new String[][] { { "[abc][abc][abc]", "acb", "abe", "bca", "b" },
-                { "[0-9]{3}[A-Z]{3}", "123VBD", "123A1B", "45ABC", "23AQ" }, { "1?2?3?", "13", "43", "", "21" },
-                { "[A|B][^a-z]*", "A123", "Ba1", "AA", "C" },
-                {"^He[^y]*","Hello","Hey","He","Hi"} };
+        String[][] input = new String[][] { 
+            { "[abc][abc][abc]", "acb", "abe", "bca", "b" },
+            { "[0-9]{3}[A-Z]{3}", "123VBD", "123A1B", "45ABC", "23AQ" }, 
+            { "1?2?3?", "13", "43", "", "21" },
+            { "[A|B][^a-z]*", "A123", "Ba1", "AA", "C" },
+            {"^He[^y]*","Hello","Hey","He","Hi"} };
 
         for (int i = 0; i < input.length; i++) {
             m = Pattern.matches(input[i][0], input[i][1]);
